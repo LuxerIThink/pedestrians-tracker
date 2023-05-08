527606b66d89502ab85c63e32a26ab222f2b28f7
LuxerIThink <luxerthegamer@gmail.com>
2023-05-08 21:33:03 +0200

    Change get path to func

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 
-if __name__ == "__main__":
+
+def get_dataset_path():
     if len(sys.argv) < 2:
-        print("Podaj ścieżkę do pliku jako argument!")
+        print("Add path to dataset as first argument!")
         sys.exit(1)
-
     file_path = sys.argv[1]
+    return file_path
 
-    print("Ścieżka do pliku:", file_path)
+
+if __name__ == "__main__":
+    get_dataset_path()
