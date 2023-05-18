40ac8fb4c1178da38e2776932edf3a4c8645f784
LuxerIThink <luxerthegamer@gmail.com>
2023-05-18 17:37:39 +0200

    Add load path argument

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import cv2
 import sys
 
-print("Hello World!")
+
+def get_dataset_path():
+    if len(sys.argv) < 2:
+        print("Add path to dataset as first argument!")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    return file_path
+
+
+if __name__ == '__main__':
+    path = get_dataset_path()
+    print(path)
