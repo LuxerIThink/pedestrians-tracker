e9ccf0da2847553755f4e039d2349f0364d4f9bf
LuxerIThink <eliteluxer@gmail.com>
2023-06-09 15:56:59 +0200

    Fix correct solution ratio

--- test_main.py
+++ test_main.py
@@ -1,6 +1,8 @@
 import pytest
-from test.utils import extract_solution
+import numpy as np
+from test.utils import load_text_file
 from subprocess import check_output
+from main import PersonTracker
 
 
 class TestMainFile:
@@ -14,23 +16,20 @@ class TestMainFile:
         return run_main_with_args
 
     @pytest.mark.parametrize("main_path, output_file, template_file, train_dataset, min_percentage",
-                             [("main.py", "output.txt", "train_dataset/bboxes_gt.txt", "train_dataset", 50.0)])
+                             [("main.py", "output.txt", "test/solution.txt", "train_dataset", 85.0)])
     def test_run_test(self, run_main, main_path, output_file, template_file, train_dataset, min_percentage):
         _ = run_main(main_path, train_dataset, output_file)
 
-        with open(output_file, 'r') as f:
-            output_content = f.read().strip()
-
-        template_content = extract_solution(template_file)
-
-        output_numbers = [list(map(int, line.split())) for line in output_content.split('\n')]
-        template_numbers = [list(map(int, line.split())) for line in template_content.split('\n')]
+        template_numbers = load_text_file(template_file)
+        output_numbers = load_text_file(output_file)
 
         # Count the total number of numbers and the number of correct numbers
         total_numbers = sum(len(sublist) for sublist in template_numbers)
         correct_numbers = sum(1 for sublist_out, sublist_template in zip(output_numbers, template_numbers)
-                              for num_out, num_template in zip(sublist_out, sublist_template) if num_out == num_template)
+                              for num_out, num_template in zip(sublist_out, sublist_template)
+                              if num_out == num_template)
 
         percentage_correct = (correct_numbers / total_numbers) * 100
 
-        assert percentage_correct > min_percentage
+        # print(percentage_correct)
+        assert percentage_correct >= min_percentage
